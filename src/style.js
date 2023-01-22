0c36c8c013656ce8400ed5febfa3cf5e4fc78ff1
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-01-22 18:30:54 +0100

    up

--- src/style.js
+++ src/style.js
@@ -14,7 +14,7 @@ const styles = {
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
 
-    glassContainer: "bg-white rounded-[30px] shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10",
+    glassContainer: "bg-white rounded-[30px] shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30",
     backgroundGradient: "bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-indigo-50 to-violet-200",
     buttonGradient: "bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-indigo-200 to-violet-100",
   };
